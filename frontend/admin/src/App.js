173e9d9b39ce73c7d64d607f7e769ead22262925
Duong Van Nhat <nhatn42003bn@gmail.com>
2024-11-03 17:27:08 +0700

    config Routes

--- frontend/admin/src/App.js
+++ frontend/admin/src/App.js
@@ -1,9 +1,45 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./style/dark.scss";
 
 function App() {
-  
-  return (
-    <h1>hihi</h1>
-  );
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/">
+                        <Route index element={<h1>homepage</h1>} />
+                        <Route path="users">
+                            <Route index element={<h1>userpage</h1>} />
+                            <Route
+                                path=":userId"
+                                element={<h1>userpage/userId</h1>}
+                            />
+                            <Route path="new" element={<h1>newpage</h1>} />
+                        </Route>
+                        <Route path="hotels">
+                            <Route index element={<h1>hotelspage</h1>} />
+                            <Route
+                                path=":productId"
+                                element={<h1>hotelspage/productId</h1>}
+                            />
+                            <Route
+                                path="new"
+                                element={<h1>hotelspage/new</h1>}
+                            />
+                        </Route>
+                        <Route path="rooms">
+                            <Route index element={<h1>rooms</h1>} />
+                            <Route
+                                path=":productId"
+                                element={<h1>rooms/productId</h1>}
+                            />
+                            <Route path="new" element={<h1>rooms/new</h1>} />
+                        </Route>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
