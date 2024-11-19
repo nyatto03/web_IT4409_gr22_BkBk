10f3abbf88e7b0a693984ca3bce0a5273af330ad
Phú Phạm <phamnguyenphu14@gmail.com>
2024-11-19 10:48:31 +0700

    Update room

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,6 +1,29 @@
-import express from "express"
+import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+  updateRoom,
+  updateRoomAvailability,
+} from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
+//create
+router.post("/:hotelid", verifyAdmin, createRoom);
 
+//update
+router.put("/availability/:id", updateRoomAvailability);
+router.put("/:id", verifyAdmin, updateRoom);
 
-export default router
+//delete
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+
+//get
+router.get("/:id", getRoom);
+
+//getall
+router.get("/", getRooms);
+
+export default router;
