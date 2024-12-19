970720de346cbcdfde2039fee2193f4895c33eb8
Phú Phạm <phamnguyenphu14@gmail.com>
2024-12-19 14:34:39 +0700

    Add files via upload

--- api/utils/authMiddleware.js
+++ api/utils/authMiddleware.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import jwt from 'jsonwebtoken';
 import User from '../models/mUser.js';
 import dotenv from 'dotenv';
@@ -37,43 +36,3 @@ export const authMiddleware = async (req, res, next) => {
     res.status(401).send({ error: 'Vui lòng xác thực' });
   }
 };
-=======
-import jwt from 'jsonwebtoken';
-import User from '../models/mUser.js';
-import dotenv from 'dotenv';
-
-dotenv.config();
-const secretKey = process.env.JWT;
-
-export const authMiddleware = async (req, res, next) => {
-  try {
-    // Kiểm tra xem header Authorization có tồn tại không
-    const token = req.header('Authorization')?.replace('Bearer ', '');
-    
-    // Nếu không có token
-    if (!token) {
-      return res.status(401).send({ error: 'Authentication token missing' });
-    }
-
-    // Giải mã và xác thực JWT
-    const decoded = jwt.verify(token, secretKey);
-
-    // Tìm người dùng dựa trên _id đã được giải mã trong token
-    const user = await User.findOne({ _id: decoded._id });
-
-    // Nếu không tìm thấy người dùng, trả về lỗi
-    if (!user) {
-      return res.status(401).send({ error: 'User not found' });
-    }
-
-    // Gán đối tượng người dùng vào request để các route/middleware phía sau có thể sử dụng
-    req.user = user;
-
-    // Tiến hành chuyển sang middleware hoặc route handler tiếp theo
-    next();
-  } catch (error) {
-    console.error('Lỗi xác thực:', error); // Ghi lại lỗi để tiện theo dõi
-    res.status(401).send({ error: 'Vui lòng xác thực' });
-  }
-};
->>>>>>> 7f612c9061281e96160f365097d9dba44d2b0ed4
