1ad0e23fb6ffe963ab3af568e1180858b88e46b8
Duong Van Nhat <nhatn42003bn@gmail.com>
2024-12-26 01:46:37 +0700

    complete Dashboard

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -16,7 +16,7 @@ const CustomSidebar = ({ selectedKey, onSelect }) => {
         <Menu.Item key="1" icon={<AppstoreAddOutlined />}>Dashboard</Menu.Item>
         <Menu.Item key="2" icon={<UserOutlined />}>Users</Menu.Item>
         <Menu.Item key="3" icon={<FileDoneOutlined />}>Orders</Menu.Item>
-        <Menu.Item key="4" icon={<ApartmentOutlined />}>Rooms</Menu.Item> {/* Thêm mục Rooms */}
+        <Menu.Item key="4" icon={<ApartmentOutlined />}>Rooms</Menu.Item>
       </Menu>
     </Sider>
   );
--- frontend/src/pages/AdminPage.js
+++ frontend/src/pages/AdminPage.js
@@ -1,56 +1,57 @@
 import React, { useState } from 'react';
 import { Layout } from 'antd';
-import Header from '../components/Header'; 
-import Sidebar from '../components/Sidebar';  
-import RoomTable from '../components/RoomTable';  
-import UserTable from '../components/UserTable';  
-import OrderTable from '../components/OrderTable'; 
+import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
+import RoomTable from '../components/RoomTable';
+import UserTable from '../components/UserTable';
+import OrderTable from '../components/OrderTable';
+import Dashboard from '../components/Dashboard';
 
 const { Content } = Layout;
 
 const AdminPage = () => {
-  const savedSelectedKey = localStorage.getItem('selectedKey') || '1'; 
+    const savedSelectedKey = localStorage.getItem('selectedKey') || '1';
 
-  const [selectedKey, setSelectedKey] = useState(savedSelectedKey);
+    const [selectedKey, setSelectedKey] = useState(savedSelectedKey);
 
-  const handleSelectMenu = (e) => {
-    const key = e.key;
-    setSelectedKey(key);
-    localStorage.setItem('selectedKey', key); 
-  };
+    const handleSelectMenu = (e) => {
+        const key = e.key;
+        setSelectedKey(key);
+        localStorage.setItem('selectedKey', key);
+    };
 
-  return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Sidebar selectedKey={selectedKey} onSelect={handleSelectMenu} />
-      <Layout style={{ marginLeft: 200 }}>
-        <Header />
-        <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
-          <div style={{ padding: 24, minHeight: 360 }}>
-            {selectedKey === '1' && (
-              <>
-                {/* Dashboard content */}
-              </>
-            )}
-            {selectedKey === '2' && (
-              <>
-                <UserTable />
-              </>
-            )}
-            {selectedKey === '3' && (
-              <>
-                <OrderTable />
-              </>
-            )}
-            {selectedKey === '4' && (
-              <>
-                <RoomTable />
-              </>
-            )}
-          </div>
-        </Content>
-      </Layout>
-    </Layout>
-  );
+    return (
+        <Layout style={{ minHeight: '100vh' }}>
+            <Sidebar selectedKey={selectedKey} onSelect={handleSelectMenu} />
+            <Layout style={{ marginLeft: 200 }}>
+                <Header />
+                <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+                    <div style={{ padding: 24, minHeight: 360 }}>
+                        {selectedKey === '1' && (
+                            <>
+                                <Dashboard />
+                            </>
+                        )}
+                        {selectedKey === '2' && (
+                            <>
+                                <UserTable />
+                            </>
+                        )}
+                        {selectedKey === '3' && (
+                            <>
+                                <OrderTable />
+                            </>
+                        )}
+                        {selectedKey === '4' && (
+                            <>
+                                <RoomTable />
+                            </>
+                        )}
+                    </div>
+                </Content>
+            </Layout>
+        </Layout>
+    );
 };
 
 export default AdminPage;
