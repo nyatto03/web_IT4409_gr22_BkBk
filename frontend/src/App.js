5c6355e2162970fdbc0767c7f38ce547d43436eb
Nguyen Van Nham <nhampmq@gmail.com>
2024-12-26 06:56:23 +0700

    call api to profile, link to map, add images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,18 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import routesConfig from './routes';
-import "./assets/styles/common.scss"
+import './assets/styles/common.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        {routesConfig.map(({ path, element }) => (
-          <Route key={path} path={path} element={element} />
-        ))}
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                {routesConfig.map(({ path, element }) => (
+                    <Route key={path} path={path} element={element} />
+                ))}
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
